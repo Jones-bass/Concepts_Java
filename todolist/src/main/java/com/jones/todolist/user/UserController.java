796240692363d3f51b397928740740b78279346e
jones-bass <jonesbass.tb@gmail.com>
2024-02-19 07:57:43 -0300

    Validando User + BCrypt

--- todolist/src/main/java/com/jones/todolist/user/UserController.java
+++ todolist/src/main/java/com/jones/todolist/user/UserController.java
@@ -3,15 +3,33 @@ package com.jones.todolist.user;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    @PostMapping("/")    
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
+    @Autowired
+    private IUserRepository userRepository;
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
+        
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
